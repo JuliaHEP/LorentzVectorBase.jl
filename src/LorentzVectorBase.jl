f4b353b1b4711750c939f1bf4734afa33cd5103f
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-10-08 10:29:36 +0200

    removed old docstrings

--- src/LorentzVectorBase.jl
+++ src/LorentzVectorBase.jl
@@ -2,6 +2,7 @@ module LorentzVectorBase
 
 export coordinate_system, coordinate_names
 
+include("getter.jl")
 include("interface.jl")
 include("coordinate_systems/XYZT.jl")
 include("coordinate_systems/PtEtaPhiM.jl")
