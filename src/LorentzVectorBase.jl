7a85056f6b9b74f384869491f93cd84f0e8989cd
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-10-08 13:47:00 +0200

    fixed tests for XYZT

--- src/LorentzVectorBase.jl
+++ src/LorentzVectorBase.jl
@@ -5,6 +5,7 @@ export coordinate_system, coordinate_names
 include("getter.jl")
 include("interface.jl")
 include("coordinate_systems/XYZT.jl")
+include("coordinate_systems/PxPyPzE.jl")
 include("coordinate_systems/PtEtaPhiM.jl")
 
 end
