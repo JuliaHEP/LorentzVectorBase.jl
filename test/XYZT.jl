764b2d7ddb794fb9df1d2b1a1773a5ae0d213337
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-10-08 13:47:00 +0200

    fixed tests for XYZT

--- test/XYZT.jl
+++ test/XYZT.jl
@@ -13,106 +13,101 @@ struct CustomLVector
 end
 
 LorentzVectorBase.coordinate_system(::CustomLVector) = LorentzVectorBase.XYZT()
-LorentzVectorBase.x(mom::CustomLVector) = mom.x
-LorentzVectorBase.y(mom::CustomLVector) = mom.y
-LorentzVectorBase.z(mom::CustomLVector) = mom.z
-LorentzVectorBase.t(mom::CustomLVector) = mom.t
+LorentzVectorBase.x(lv::CustomLVector) = lv.x
+LorentzVectorBase.y(lv::CustomLVector) = lv.y
+LorentzVectorBase.z(lv::CustomLVector) = lv.z
+LorentzVectorBase.t(lv::CustomLVector) = lv.t
 
 x, y, z = rand(RNG, 3)
-m = rand(RNG)
-t = sqrt(x^2 + y^2 + z^2 + m^2)
-mom_onshell = CustomLVector(x, y, z, t)
-mom_zero = CustomLVector(0.0, 0.0, 0.0, 0.0)
-mom_offshell = CustomLVector(0.0, 0.0, m, 0.0)
+t = sqrt(1 + x^2 + y^2 + z^2) # ensure a positive p*p
+lvec_non_zero = CustomLVector(x, y, z, t)
+lvec_zero = CustomLVector(0.0, 0.0, 0.0, 0.0)
 
-@testset "spatial_magnitude consistence" for mom in [mom_onshell, mom_offshell, mom_zero]
+@testset "spatial_magnitude consistence" for lvec in [lvec_non_zero, lvec_zero]
   @test isapprox(
-    LorentzVectorBase.spatial_magnitude(mom),
-    sqrt(LorentzVectorBase.spatial_magnitude2(mom)),
+    LorentzVectorBase.spatial_magnitude(lvec),
+    sqrt(LorentzVectorBase.spatial_magnitude2(lvec)),
   )
 end
 
 @testset "spatial_magnitude values" begin
-  @test isapprox(LorentzVectorBase.spatial_magnitude2(mom_onshell), x^2 + y^2 + z^2)
-  @test isapprox(LorentzVectorBase.spatial_magnitude(mom_onshell), sqrt(x^2 + y^2 + z^2))
+  @test isapprox(LorentzVectorBase.spatial_magnitude2(lvec_non_zero), x^2 + y^2 + z^2)
+  @test isapprox(LorentzVectorBase.spatial_magnitude(lvec_non_zero), sqrt(x^2 + y^2 + z^2))
 end
 
-@testset "mass consistence" for mom_on in [mom_onshell, mom_zero]
-  @test isapprox(LorentzVectorBase.mass(mom_on), sqrt(LorentzVectorBase.mass2(mom_on)))
-  @test LorentzVectorBase.invariant_mass2(mom_on) == LorentzVectorBase.mass2(mom_on)
-  @test LorentzVectorBase.invariant_mass(mom_on) == LorentzVectorBase.mass(mom_on)
+@testset "mass consistence" for lvec in [lvec_non_zero, lvec_zero]
+  @test isapprox(LorentzVectorBase.mass(lvec), sqrt(LorentzVectorBase.mass2(lvec)))
+  @test LorentzVectorBase.invariant_mass2(lvec) == LorentzVectorBase.mass2(lvec)
+  @test LorentzVectorBase.invariant_mass(lvec) == LorentzVectorBase.mass(lvec)
   @test isapprox(
-    LorentzVectorBase.invariant_mass(mom_on),
-    sqrt(LorentzVectorBase.invariant_mass2(mom_on)),
+    LorentzVectorBase.invariant_mass(lvec), sqrt(LorentzVectorBase.invariant_mass2(lvec))
   )
 end
 
 @testset "mass value" begin
-  @test isapprox(LorentzVectorBase.mass2(mom_onshell), t^2 - (x^2 + y^2 + z^2))
-  @test isapprox(LorentzVectorBase.mass(mom_onshell), sqrt(t^2 - (x^2 + y^2 + z^2)))
+  @test isapprox(LorentzVectorBase.mass2(lvec_non_zero), t^2 - (x^2 + y^2 + z^2))
+  @test isapprox(LorentzVectorBase.mass(lvec_non_zero), sqrt(t^2 - (x^2 + y^2 + z^2)))
 
-  @test isapprox(LorentzVectorBase.mass(mom_onshell), m)
-  @test isapprox(LorentzVectorBase.mass(mom_offshell), -m)
-  @test isapprox(LorentzVectorBase.mass(mom_zero), 0.0)
+  @test isapprox(LorentzVectorBase.mass(lvec_zero), 0.0)
 end
 
 @testset "momentum components" begin
-  @test LorentzVectorBase.t(mom_onshell) == t
-  @test LorentzVectorBase.x(mom_onshell) == x
-  @test LorentzVectorBase.y(mom_onshell) == y
-  @test LorentzVectorBase.z(mom_onshell) == z
+  @test LorentzVectorBase.t(lvec_non_zero) == t
+  @test LorentzVectorBase.x(lvec_non_zero) == x
+  @test LorentzVectorBase.y(lvec_non_zero) == y
+  @test LorentzVectorBase.z(lvec_non_zero) == z
 
-  @test isapprox(LorentzVectorBase.boost_beta(mom_onshell), sqrt(x^2 + y^2 + z^2) / t)
+  @test isapprox(LorentzVectorBase.boost_beta(lvec_non_zero), sqrt(x^2 + y^2 + z^2) / t)
   @test isapprox(
-    LorentzVectorBase.boost_gamma(mom_onshell),
-    1 / sqrt(1.0 - LorentzVectorBase.boost_beta(mom_onshell)^2),
+    LorentzVectorBase.boost_gamma(lvec_non_zero),
+    1 / sqrt(1.0 - LorentzVectorBase.boost_beta(lvec_non_zero)^2),
   )
 
-  @test LorentzVectorBase.t(mom_zero) == 0.0
-  @test LorentzVectorBase.x(mom_zero) == 0.0
-  @test LorentzVectorBase.y(mom_zero) == 0.0
-  @test LorentzVectorBase.z(mom_zero) == 0.0
-  @test isapprox(LorentzVectorBase.boost_beta(mom_zero), 0.0)
-  @test isapprox(LorentzVectorBase.boost_gamma(mom_zero), 1.0)
+  @test LorentzVectorBase.t(lvec_zero) == 0.0
+  @test LorentzVectorBase.x(lvec_zero) == 0.0
+  @test LorentzVectorBase.y(lvec_zero) == 0.0
+  @test LorentzVectorBase.z(lvec_zero) == 0.0
+  @test isapprox(LorentzVectorBase.boost_beta(lvec_zero), 0.0)
+  @test isapprox(LorentzVectorBase.boost_gamma(lvec_zero), 1.0)
 end
 
 @testset "transverse coordiantes value" begin
-  @test isapprox(LorentzVectorBase.pt2(mom_onshell), x^2 + y^2)
-  @test isapprox(LorentzVectorBase.pt(mom_onshell), sqrt(x^2 + y^2))
-  @test isapprox(LorentzVectorBase.mt2(mom_onshell), t^2 - z^2)
-  @test isapprox(LorentzVectorBase.mt(mom_onshell), sqrt(t^2 - z^2))
-  @test isapprox(LorentzVectorBase.mt(mom_offshell), -m)
+  @test isapprox(LorentzVectorBase.pt2(lvec_non_zero), x^2 + y^2)
+  @test isapprox(LorentzVectorBase.pt(lvec_non_zero), sqrt(x^2 + y^2))
+  @test isapprox(LorentzVectorBase.mt2(lvec_non_zero), t^2 - z^2)
+  @test isapprox(LorentzVectorBase.mt(lvec_non_zero), sqrt(t^2 - z^2))
 
-  @test isapprox(LorentzVectorBase.rapidity(mom_onshell), 0.5 * log((t + z) / (t - z)))
+  @test isapprox(LorentzVectorBase.rapidity(lvec_non_zero), 0.5 * log((t + z) / (t - z)))
 
-  @test isapprox(LorentzVectorBase.pt2(mom_zero), 0.0)
-  @test isapprox(LorentzVectorBase.pt(mom_zero), 0.0)
-  @test isapprox(LorentzVectorBase.mt2(mom_zero), 0.0)
-  @test isapprox(LorentzVectorBase.mt(mom_zero), 0.0)
+  @test isapprox(LorentzVectorBase.pt2(lvec_zero), 0.0)
+  @test isapprox(LorentzVectorBase.pt(lvec_zero), 0.0)
+  @test isapprox(LorentzVectorBase.mt2(lvec_zero), 0.0)
+  @test isapprox(LorentzVectorBase.mt(lvec_zero), 0.0)
 end
 
-@testset "spherical coordiantes consistence" for mom_on in [mom_onshell, mom_zero]
+@testset "spherical coordiantes consistence" for lvec in [lvec_non_zero, lvec_zero]
   @test isapprox(
-    LorentzVectorBase.cos_theta(mom_on), cos(LorentzVectorBase.polar_angle(mom_on))
+    LorentzVectorBase.cos_theta(lvec), cos(LorentzVectorBase.polar_angle(lvec))
   )
-  @test isapprox(LorentzVectorBase.cos_phi(mom_on), cos(LorentzVectorBase.phi(mom_on)))
-  @test isapprox(LorentzVectorBase.sin_phi(mom_on), sin(LorentzVectorBase.phi(mom_on)))
+  @test isapprox(LorentzVectorBase.cos_phi(lvec), cos(LorentzVectorBase.phi(lvec)))
+  @test isapprox(LorentzVectorBase.sin_phi(lvec), sin(LorentzVectorBase.phi(lvec)))
 end
 
 @testset "spherical coordiantes values" begin
   @test isapprox(
-    LorentzVectorBase.polar_angle(mom_onshell), atan(LorentzVectorBase.pt(mom_onshell), z)
+    LorentzVectorBase.polar_angle(lvec_non_zero),
+    atan(LorentzVectorBase.pt(lvec_non_zero), z),
   )
-  @test isapprox(LorentzVectorBase.polar_angle(mom_zero), 0.0)
+  @test isapprox(LorentzVectorBase.polar_angle(lvec_zero), 0.0)
 
-  @test isapprox(LorentzVectorBase.phi(mom_onshell), atan(y, x))
-  @test isapprox(LorentzVectorBase.phi(mom_zero), 0.0)
+  @test isapprox(LorentzVectorBase.phi(lvec_non_zero), atan(y, x))
+  @test isapprox(LorentzVectorBase.phi(lvec_zero), 0.0)
 end
 
 @testset "light-cone coordiantes" begin
-  @test isapprox(LorentzVectorBase.plus_component(mom_onshell), 0.5 * (t + z))
-  @test isapprox(LorentzVectorBase.minus_component(mom_onshell), 0.5 * (t - z))
+  @test isapprox(LorentzVectorBase.plus_component(lvec_non_zero), 0.5 * (t + z))
+  @test isapprox(LorentzVectorBase.minus_component(lvec_non_zero), 0.5 * (t - z))
 
-  @test isapprox(LorentzVectorBase.plus_component(mom_zero), 0.0)
-  @test isapprox(LorentzVectorBase.minus_component(mom_zero), 0.0)
+  @test isapprox(LorentzVectorBase.plus_component(lvec_zero), 0.0)
+  @test isapprox(LorentzVectorBase.minus_component(lvec_zero), 0.0)
 end
