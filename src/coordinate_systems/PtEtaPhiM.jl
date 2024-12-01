cc082b2a41270468d2fa4a973282ab29b087869d
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-12-01 21:38:16 +0100

    back to space 2

--- src/coordinate_systems/PtEtaPhiM.jl
+++ src/coordinate_systems/PtEtaPhiM.jl
@@ -24,39 +24,39 @@ py(::PtEtaPhiM, v) = pt(v) * sin(phi(v))
 pz(::PtEtaPhiM, v) = pt(v) * sinh(eta(v))
 
 function E(::PtEtaPhiM, mom)
-    return sqrt(px(mom)^2 + py(mom)^2 + pz(mom)^2 + invariant_mass2(mom))
+  return sqrt(px(mom)^2 + py(mom)^2 + pz(mom)^2 + invariant_mass2(mom))
 end
 
 @inline function spatial_magnitude2(::PtEtaPhiM, mom)
-    return pt(mom)^2 * (1 + sinh(eta(mom))^2)
+  return pt(mom)^2 * (1 + sinh(eta(mom))^2)
 end
 
 @inline function spatial_magnitude(::PtEtaPhiM, mom)
-    return sqrt(spatial_magnitude2(mom))
+  return sqrt(spatial_magnitude2(mom))
 end
 
 @inline function mass2(::PtEtaPhiM, mom)
-    return mass(mom)^2
+  return mass(mom)^2
 end
 
 @inline function boost_beta(::PtEtaPhiM, mom)
-    en = energy(mom)
-    rho = spatial_magnitude(mom)
-    if !iszero(en)
-        rho / en
-    elseif iszero(rho)
-        return zero(rho)
-    else
-        throw(
-            ArgumentError(
-                "There is no beta for a four-momentum with vanishing time/energy component",
-            ),
-        )
-    end
+  en = energy(mom)
+  rho = spatial_magnitude(mom)
+  if !iszero(en)
+    rho / en
+  elseif iszero(rho)
+    return zero(rho)
+  else
+    throw(
+      ArgumentError(
+        "There is no beta for a four-momentum with vanishing time/energy component"
+      ),
+    )
+  end
 end
 
 @inline function boost_gamma(::PtEtaPhiM, mom)
-    return inv(sqrt(one(energy(mom)) - boost_beta(mom)^2))
+  return inv(sqrt(one(energy(mom)) - boost_beta(mom)^2))
 end
 
 ########################
@@ -64,27 +64,27 @@ end
 ########################
 
 @inline function pt2(::PtEtaPhiM, mom)
-    return pt(mom)^2
+  return pt(mom)^2
 end
 
 @inline function mt2(::PtEtaPhiM, mom)
-    return energy(mom)^2 - pz(mom)^2
+  return energy(mom)^2 - pz(mom)^2
 end
 
 function mt(::PtEtaPhiM, mom)
-    mT2 = mt2(mom)
-    if mT2 < zero(mT2)
-        # add optional waring: negative transverse mass -> -sqrt(-mT2) is returned.
-        -sqrt(-mT2)
-    else
-        sqrt(mT2)
-    end
+  mT2 = mt2(mom)
+  if mT2 < zero(mT2)
+    # add optional waring: negative transverse mass -> -sqrt(-mT2) is returned.
+    -sqrt(-mT2)
+  else
+    sqrt(mT2)
+  end
 end
 
 @inline function rapidity(::PtEtaPhiM, mom)
-    E = energy(mom)
-    pz1 = pz(mom)
-    return 0.5 * log((E + pz1) / (E - pz1))
+  E = energy(mom)
+  pz1 = pz(mom)
+  return 0.5 * log((E + pz1) / (E - pz1))
 end
 
 #######################
@@ -92,33 +92,33 @@ end
 #######################
 
 @inline function polar_angle(::PtEtaPhiM, mom)
-    η = eta(mom)
-    r = spatial_magnitude(mom)
-    return iszero(r) && iszero(pz(mom)) ? zero(pz(mom)) : 2 * atan(exp(-η))
+  η = eta(mom)
+  r = spatial_magnitude(mom)
+  return iszero(r) && iszero(pz(mom)) ? zero(pz(mom)) : 2 * atan(exp(-η))
 end
 
 @inline function cos_theta(::PtEtaPhiM, mom)
-    r = spatial_magnitude(mom)
-    return iszero(r) ? one(px(mom)) : pz(mom) / r
+  r = spatial_magnitude(mom)
+  return iszero(r) ? one(px(mom)) : pz(mom) / r
 end
 
 function cos_phi(::PtEtaPhiM, mom)
-    pT = pt(mom)
-    return iszero(pT) ? one(pT) : px(mom) / pT
+  pT = pt(mom)
+  return iszero(pT) ? one(pT) : px(mom) / pT
 end
 
 function sin_phi(::PtEtaPhiM, mom)
-    pT = pt(mom)
-    return iszero(pT) ? zero(pT) : py(mom) / pT
+  pT = pt(mom)
+  return iszero(pT) ? zero(pT) : py(mom) / pT
 end
 
 ########################
 # light cone coordinates
 ########################
 @inline function plus_component(::PtEtaPhiM, mom)
-    return 0.5 * (energy(mom) + pz(mom))
+  return 0.5 * (energy(mom) + pz(mom))
 end
 
 @inline function minus_component(::PtEtaPhiM, mom)
-    return 0.5 * (energy(mom) - pz(mom))
+  return 0.5 * (energy(mom) - pz(mom))
 end
